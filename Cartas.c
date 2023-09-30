d0fd3dc15686f294a39ba3b205ca05047930d2f3
Pasita24 <benjapaz024@gmail.com>
2023-09-29 22:43:54 -0300

    Actualizacion
    Archivo txt

--- Cartas.c
+++ Cartas.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
+
+#define Max_Cartas 30
 
 struct Guardian {
     char nombre[50];
@@ -9,6 +12,7 @@ struct Guardian {
     int PD;
     char tipo[20];
 };
+
 struct Carta {
     struct Guardian guardian;
     int enCampoBatalla;
@@ -19,9 +23,16 @@ struct Jugador {
     struct Carta mano[15];
     int numCartasEnMano;
 };
+
+void cargarCartasDesdeArchivo(struct Guardian cartas[], int *numCartas);
+void mostrarMenu();
+
 int main() {
-    cargarCartasDesdeArchivo();
-    
+    struct Guardian cartas[Max_Cartas];
+    int numCartas = 0;
+
+    cargarCartasDesdeArchivo(cartas, &numCartas);
+
     int opcion;
     do {
         mostrarMenu();
@@ -29,12 +40,11 @@ int main() {
         scanf("%d", &opcion);
         switch (opcion) {
             case 1:
-                crearNuevaCarta();
                 break;
             case 2:
-                iniciarJuego();
                 break;
             case 3:
+
                 break;
             case 4:
                 printf("Saliendo del juego.\n");
@@ -47,7 +57,34 @@ int main() {
     return 0;
 }
 
-void cargarCartasDesdeArchivo() {
+void cargarCartasDesdeArchivo(struct Guardian cartas[], int *numCartas) {
+    FILE *archivo = fopen("Cartas.txt", "r");
+    if (archivo == NULL) {
+        perror("Error al abrir el archivo");
+        exit(1);
+    }
+
+    char linea[100];
+    char *token;
+
+    fgets(linea, sizeof(linea), archivo);
+
+    while (fgets(linea, sizeof(linea), archivo) != NULL) {
+        token = strtok(linea, ",");
+        strcpy(cartas[*numCartas].tipo, token);
+        token = strtok(NULL, ",");
+        strcpy(cartas[*numCartas].nombre, token);
+        token = strtok(NULL, ",");
+        cartas[*numCartas].PV = atoi(token);
+        token = strtok(NULL, ",");
+        cartas[*numCartas].PA = atoi(token);
+        token = strtok(NULL, ",");
+        cartas[*numCartas].PD = atoi(token);
+
+        (*numCartas)++;
+    }
+
+    fclose(archivo);
 }
 
 void mostrarMenu() {
@@ -57,4 +94,3 @@ void mostrarMenu() {
     printf("3. Historial de la partida\n");
     printf("4. Salir\n");
 }
-
