9fcce706048f90cfe553ae6ac1abe20b6ebffe40
Pasita24 <benjapaz024@gmail.com>
2023-10-03 22:53:40 -0300

    Actualizacion

--- Cartas.c
+++ Cartas.c
@@ -110,6 +110,8 @@ void revolverCartas(struct Guardian cartas[], int numCartas) {
     printf("..\n");
     printf(".\n");
     printf(".\n");
+    printf(".\n");
+
     for (int i = numCartas - 1; i > 0; i--) {
         int j = rand() % (i + 1);
 
